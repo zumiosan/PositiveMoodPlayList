d18d3da4d5a121070ea439a6a4febe256b26eaf2
zumio <42907343+zumiosan@users.noreply.github.com>
2021-08-19 22:21:37 +0900

    実験情報のモデルを追加

--- src/PositiveMoodPlayList/app/models.py
+++ src/PositiveMoodPlayList/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class ExperimentInfo(models.Model):
+    """
+    実験に使う情報
+    """
+    user_id = models.CharField(verbose_name='ユーザID', max_length=255)
+    ex_id = models.IntegerField(verbose_name='実験番号', default=0)
+    playlist_type = models.IntegerField(verbose_name='プレイリストのタイプ', default=0)
+    playlist_mid = ArrayField(models.IntegerField(), verbose_name='プレイリストの楽曲', default=[])
+
