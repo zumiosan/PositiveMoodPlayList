120dddfdd53b838b7af88eb9db8d4973678a7c32
zumio <42907343+zumiosan@users.noreply.github.com>
2021-09-07 17:43:55 +0900

    実験情報の更新機能を作成

--- src/PositiveMoodPlayList/playlist/views.py
+++ src/PositiveMoodPlayList/playlist/views.py
@@ -3,25 +3,34 @@ from rest_framework.response import Response
 from rest_framework import permissions, status
 from .models import ExperimentInfo
 from .modules import CreatePlayList
+from .serializers import ExperimentInfoSerializer
+from django.core.exceptions import ObjectDoesNotExist
 
 
-class GetExperimentInfoView(APIView):
+class ExperimentInfoView(APIView):
     """
-    実験情報を取得する．
+    実験情報に関するView．
     """
     permission_classes = [permissions.IsAuthenticated]
 
     def get(self, request):
+        """実験情報の取得を行う"""
         query_set = ExperimentInfo.objects.filter(username=request.user.username).order_by('ex_id')
-        data = []
-        for res in query_set:
-            tmp = {
-                'ex_id': res.ex_id,
-                'is_finished': res.is_finished
-            }
-            data.append(tmp)
+        serializer = ExperimentInfoSerializer(instance=query_set, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
 
-        return Response(data=data, status=status.HTTP_200_OK)
+    def put(self, request):
+        """実験情報の更新を行う"""
+        if type(request.data) is not dict:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        try:
+            query_set = ExperimentInfo.objects.get(username=request.user.username, ex_id=request.data['ex_id'])
+        except (ObjectDoesNotExist, KeyError):
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        serializer = ExperimentInfoSerializer(instance=query_set, data=request.data)
+        serializer.is_valid()
+        serializer.save()
+        return Response(status=status.HTTP_200_OK)
 
 
 class CreatePlaylistView(APIView):
