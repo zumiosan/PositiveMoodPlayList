0b9dd3125a2dfb670e1cbfb663fa6c01975123b6
zumio <42907343+zumiosan@users.noreply.github.com>
2021-09-22 23:06:53 +0900

    オーディオプレイヤーの作成途中

--- src/PositiveMoodPlayList/playlist/front/src/components/MusicPlayer.tsx
+++ src/PositiveMoodPlayList/playlist/front/src/components/MusicPlayer.tsx
@@ -1,10 +1,39 @@
-import React, {Fragment} from "react";
+import React, { Fragment, useContext } from "react";
+import { PlayListContext } from "../index";
+import { makeStyles } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles({
+    audio: {
+        display: "none",
+    },
+    footer: {
+        width: "100%",
+        height: "10%",
+        position: "fixed",
+        bottom: "0",
+    }
+
+});
 
 export default function MusicPlayer() {
 
-  return (
-    <Fragment>
+    const classes = useStyles()
 
-    </Fragment>
-  );
+    //　プレイリストの楽曲
+    const playListContext = useContext(PlayListContext)!;
+    const [playList] = [playListContext.playList];
+
+    return (
+        <Fragment>
+            <Box component={"footer"} className={classes.footer} bgcolor={"#3f51b5"}>
+                <audio src={'sample.wav'} controls className={classes.audio}></audio>
+                <Grid container>
+
+                </Grid>
+            </Box>
+        </Fragment>
+    );
 };
--- src/PositiveMoodPlayList/playlist/front/src/index.tsx
+++ src/PositiveMoodPlayList/playlist/front/src/index.tsx
@@ -76,7 +76,6 @@ export default function App() {
                 </LoggedInContext.Provider>
             </BrowserRouter>
         </div>
-
     );
 }
 
