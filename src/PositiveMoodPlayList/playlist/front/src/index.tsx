ef845d1957acabe3a7670ecc65cd4ec5a96f978b
zumio <42907343+zumiosan@users.noreply.github.com>
2021-09-22 21:02:07 +0900

    プレイリストに使用する楽曲格納用の変数追加

--- src/PositiveMoodPlayList/playlist/front/src/index.tsx
+++ src/PositiveMoodPlayList/playlist/front/src/index.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, createContext, useState, useEffect} from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import {Switch, Route, BrowserRouter, Link} from "react-router-dom";
 import Login from "./components/Login";
@@ -6,8 +6,14 @@ import Header from "./components/Header";
 import Experiment from "./components/Experiment";
 import ExperimentDetail from "./components/ExperimentDetail";
 import {getUserInfo} from "./components/modules/apiJwt";
+import MusicPlayer from "./components/MusicPlayer";
 
 export const apiURL = 'http://localhost:8000/';
+
+interface PlayListContextInterface {
+    playList: number[]
+    setPlayList: React.Dispatch<React.SetStateAction<number[]>>
+}
 
 interface LoggedInContextInterface {
     isLoggedIn: boolean
@@ -15,17 +21,25 @@ interface LoggedInContextInterface {
 }
 
 export const LoggedInContext = createContext<LoggedInContextInterface | null>(null)
+export const PlayListContext = createContext<PlayListContextInterface | null>(null)
 
 export default function App() {
 
     //ログイン状態
     const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
 
+    // プレイリスト再生楽曲リスト
+    const [playList, setPlayList] = useState<number[]>([]);
+
     //子コンポーネントに送るもの
     const loggedInContext: LoggedInContextInterface = {
         isLoggedIn: isLoggedIn,
-        setLoggedIn: setIsLoggedIn
+        setLoggedIn: setIsLoggedIn,
     };
+    const playListContext: PlayListContextInterface = {
+        playList: playList,
+        setPlayList: setPlayList,
+    }
 
     useEffect(() => {
         (async () => {
@@ -40,20 +54,25 @@ export default function App() {
             <BrowserRouter>
                 <LoggedInContext.Provider value={loggedInContext}>
                     <Header />
-                    <Switch>
-                        <Route exact path="/">
+                    <PlayListContext.Provider value={playListContext}>
+                        <Switch>
+                            <Route exact path="/">
 
-                        </Route>
-                        <Route exact path="/login">
-                            <Login />
-                        </Route>
-                        <Route exact path={"/experiment"}>
-                            <Experiment />
-                        </Route>
-                        <Route path={"/experiment/detail/:exptInfo"}>
-                            <ExperimentDetail />
-                        </Route>
-                    </Switch>
+                            </Route>
+                            <Route exact path="/login">
+                                <Login />
+                            </Route>
+                            <Route exact path={"/experiment"}>
+                                <Experiment />
+                            </Route>
+                            <Route path={"/experiment/detail/:exptInfo"}>
+                                <ExperimentDetail />
+                            </Route>
+                        </Switch>
+                        {isLoggedIn && (
+                            <MusicPlayer />
+                        )}
+                    </PlayListContext.Provider>
                 </LoggedInContext.Provider>
             </BrowserRouter>
         </div>
