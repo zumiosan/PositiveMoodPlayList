5f563b0db09513f88a56d9a495b286510aa67676
zumio <42907343+zumiosan@users.noreply.github.com>
2021-12-15 15:40:56 +0900

    permissionの設定のデフォルトを変更

--- src/PositiveMoodPlayList/PositiveMoodPlayList/settings.py
+++ src/PositiveMoodPlayList/PositiveMoodPlayList/settings.py
@@ -149,7 +149,7 @@ SIMPLE_JWT = {
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'PositiveMoodPlayList.authenticate.CustomAuthentication',
